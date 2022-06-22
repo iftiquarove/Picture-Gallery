0f73dbd0ab200af3a8b2b2a211cae10fe423ae9e
ultron33 <78212054+ultron33@users.noreply.github.com>
2022-06-22 11:31:45 +0600

    :anchor: Anchoring related functions added

--- Picture Gallery/Helper/Utility.swift
+++ Picture Gallery/Helper/Utility.swift
@@ -6,8 +6,19 @@
 //
 
 import Foundation
+import SystemConfiguration
 import UIKit
 
 class Utlity: NSObject{
     
+    //MARK: - Height/Width related
+    public class func convertHeightMultiplier(constant : CGFloat) -> CGFloat{
+        let value = constant/896
+        return value*UIScreen.main.bounds.height
+    }
+    
+    public class func convertWidthMultiplier(constant : CGFloat) -> CGFloat{
+        let value = constant/414
+        return value*UIScreen.main.bounds.width
+    }
 }
